736a0df7959ec1b81937d8c09a72fd62d2f874de
Melissa <a016285m@student.staffs.ac.uk>
2024-06-21 18:17:00 +0100

    Tweaked button text when enabled or disabled

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -52,10 +52,10 @@ void myLookAndFeel::drawRotarySlider(juce::Graphics& g, int x, int y, int width,
         r.setSize(strWidth + 4, rswl->getTextHeight() + 2);
         r.setCentre(bounds.getCentre());
 
-        g.setColour(Colours::black);
+        g.setColour(enabled ? Colours::black : Colours::darkgrey);
         g.fillRect(r);
 
-        g.setColour(Colours::white);
+        g.setColour(enabled ? Colours::white : Colours::lightgrey);
         g.drawFittedText(text, r.toNearestInt(), juce::Justification::centred, 1);
     }
 }
